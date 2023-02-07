05c72699c65f2a13113b274583805ed8e0ff6274
delafuente <delafuente@tirant.com>
2023-02-07 23:19:23 +0100

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :movies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # Defines the root path route ("/") definir
   # root "articles#index"
 end
