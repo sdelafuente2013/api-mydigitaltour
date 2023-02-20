7aba5b0df3eb3192016098235675d84d23aab698
delafuente <delafuente@tirant.com>
2023-02-20 22:17:09 +0100

    config cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins ''
+    resource '', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], expose: ['Authorization']
   end
 end
