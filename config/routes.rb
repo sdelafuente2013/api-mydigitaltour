6efd4c497f49f3077ca40d9a1d343c26bf25c6c1
delafuente <delafuente@tirant.com>
2023-03-01 21:54:13 +0100

    add endpoint health with status ok

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   devise_for :users, defaults: { format: :json }
+  get '/health', to: 'health#show'
 end
