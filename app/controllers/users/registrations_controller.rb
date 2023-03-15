cef8118b6726cb02e4a0e0a6f4c0766d3bf95184
delafuente <delafuente@tirant.com>
2023-03-15 23:21:44 +0100

    add api/v1/login to login

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :update_sanitized_params, if: :devise_controller?
-
-  def update_sanitized_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :lastname, :role, :status])
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json }, controllers: { registrations: 'users/registrations' }
 
   # /api/v1/health
   namespace :api do
     namespace :v1 do
+      devise_for :users, defaults: { format: :json }, controllers: { registrations: 'api/v1/users/registrations' }
       get '/health', to: 'health#show'
 
       # tour
