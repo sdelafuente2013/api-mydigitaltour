3161f2804a660bfbc74f95cbc220bb0bc8934081
delafuente <delafuente@tirant.com>
2023-04-13 21:50:30 +0200

    Refactor health_spec.rb to test the new API endpoint and verify a successful response with an OK status.

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -4,10 +4,11 @@ require 'rails_helper'
 
 RSpec.describe 'Health endpoint', type: :request do
   describe 'GET /health' do
-    it 'should return OK' do
-      get '/health'
-      expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)).to eq({ 'status' => 'OK' })
+    it 'returns OK status' do
+      get '/api/v1/health'
+
+      expect(response).to be_successful
+      expect(JSON.parse(response.body)).to eq('status' => 'OK status')
     end
   end
 end
