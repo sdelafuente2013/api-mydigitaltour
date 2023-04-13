189802433136c130fff57f49aaf1f5a68d005477
delafuente <delafuente@tirant.com>
2023-04-13 14:13:49 +0200

    Update devise.rb: Reformat JWT configuration for increased readability and set expiration time to 30 minutes.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,53 +17,14 @@ Devise.setup do |config|
   config.sign_out_via = :delete
   config.navigational_formats = []
 
-  # ==> Warden configuration
-  # If you want to use other strategies, that are not supported by Devise, or
-  # change the failure app, you can configure them inside the config.warden block.
-  #
-  # config.warden do |manager|
-  #   manager.intercept_401 = false
-  #   manager.default_strategies(scope: :user).unshift :some_external_strategy
-  # end
-
-  # ==> Mountable engine configurations
-  # When using Devise inside an engine, let's call it `MyEngine`, and this engine
-  # is mountable, there are some extra configurations to be taken into account.
-  # The following options are available, assuming the engine is mounted as:
-  #
-  #     mount MyEngine, at: '/my_engine'
-  #
-  # The router that invoked `devise_for`, in the example above, would be:
-  # config.router_name = :my_engine
-  #
-  # When using OmniAuth, Devise cannot automatically set OmniAuth path,
-  # so you need to do it manually. For the users scope, it would be:
-  # config.omniauth_path_prefix = '/my_engine/users/auth'
-
-  # ==> Turbolinks configuration
-  # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
-  #
-  # ActiveSupport.on_load(:devise_failure_app) do
-  #   include Turbolinks::Controller
-  # end
-
-  # ==> Configuration for :registerable
-
-  # When set to false, does not sign a user in automatically after their password is
-  # changed. Defaults to true, so a user is signed in automatically after changing a password.
-  # config.sign_in_after_change_password = true
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials[:devise_jwt_secret]
     jwt.dispatch_requests = [
-      ['POST', %r{users/sign_in}]
+      ['POST', %r{^/login$}]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{users/sign_out}]
+      ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 15.day.to_i
-
-    jwt.request_formats = {
-      user: [:json]
-    }
+    jwt.expiration_time = 30.minutes.to_i
   end
 end
