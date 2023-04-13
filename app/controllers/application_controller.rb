ae3b5de1dcd3c7e4442c9a714cd556a5784acd78
delafuente <delafuente@tirant.com>
2023-04-13 14:11:04 +0200

    Add before action to configure permitted parameters for Devise controllers in Application Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 class ApplicationController < ActionController::API
   respond_to :json
   include ActionController::MimeResponds
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation name lastname role status])
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[email password password_confirmation name lastname role status])
+    end
 end
