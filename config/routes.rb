83a69f73ae4dcf5137d1412343b2262db99c470d
delafuente <delafuente@tirant.com>
2023-04-13 14:12:21 +0200

    Add devise for user authentication and replace tour routes with RESTful routes for index, show, destroy, update, and create in routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
+
 require 'rswag/ui'
 
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
-  # /api/v1/health
+  # devise for users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+   controllers: {
+     sessions: 'users/sessions',
+     registrations: 'users/registrations'
+   }
+
   namespace :api do
     namespace :v1 do
-      devise_for :users, defaults: { format: :json }, controllers: { registrations: 'api/v1/users/registrations' }
+      resources :tours, only: [:index, :show, :destroy, :update, :create]
       get '/health', to: 'health#show'
-
-      # tour
-      get '/tours', to: 'tours#index'
-      get 'tours/:id', to: 'tours#show'
-      delete 'tours/:id', to: 'tours#destroy'
-      patch 'tours/:id', to: 'tours#update'
-      post 'tours', to: 'tours#create'
     end
   end
 end
