07b2edeab79d55f3e5feff94373cdde0a70c54be
delafuente <delafuente@tirant.com>
2023-04-13 14:11:31 +0200

    - Delete registrations_controller.rb and its contents, as it is no longer needed."

--- app/controllers/api/v1/users/registrations_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
-  private
-    def sign_up_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name, :lastname, :role, :status)
-    end
-end
